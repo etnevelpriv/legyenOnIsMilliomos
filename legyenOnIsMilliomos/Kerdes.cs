801fb31f2da1f958de2bf2843c08ae8636947e21
Levente Rasztovits <rasztovits.levente@petrik.hu>
2025-05-09 13:00:03 +0200

    kerdes osztaly elkezdese, kesobb folytatom, mert hiba van benne

---
+++ legyenOnIsMilliomos/Kerdes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace legyenOnIsMilliomos
+{
+    internal class Kerdes
+    {
+        public string Kerdes { get; set; }
+        public List<string> Valasz { get; set; }
+        public char HelyesValasz { get; set; }
+        public string Kategoria { get; set; }
+
+        public Kerdes()
+        {
+            Valasz = new List<string>();
+        }
+
+        public Kerdes(string kerdes, List<string> valasz, char helyesValasz, string kategoria)
+        {
+            Kerdes = kerdes;
+            Valasz = valasz;
+            HelyesValasz = helyesValasz;
+            Kategoria = kategoria;
+        }
+    }
+}
