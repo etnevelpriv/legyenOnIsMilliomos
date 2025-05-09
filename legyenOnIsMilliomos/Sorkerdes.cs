0907b0ba9ffb97851e54c1a973c8a80332243699
Levente Rasztovits <rasztovits.levente@petrik.hu>
2025-05-09 12:51:28 +0200

    sorkerdes osztaly

---
+++ legyenOnIsMilliomos/Sorkerdes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace legyenOnIsMilliomos
+{
+    internal class Sorkerdes
+    {
+        public string Kerdes { get; set; }
+        public string[] Valaszok { get; set; }
+        public string HelyesSorrend { get; set; }
+        public string Tema { get; set; }
+
+        public Sorkerdes()
+        {
+            Valaszok = new string[4];
+        }
+
+        public Sorkerdes(string kerdes, string[] valaszok, string helyesSorrend, string tema)
+        {
+            Kerdes = kerdes;
+            Valaszok = valaszok;
+            HelyesSorrend = helyesSorrend;
+            Tema = tema;
+        }
+    }
+}
