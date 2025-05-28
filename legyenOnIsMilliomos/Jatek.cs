4b4d3f69fdef49cd8bc11ce671c83c85c173f7c7
Levente Rasztovits <rasztovits.levente@petrik.hu>
2025-05-28 17:39:51 +0200

    Főjáték logika

--- legyenOnIsMilliomos/Jatek.cs
+++ legyenOnIsMilliomos/Jatek.cs
@@ -12,6 +12,7 @@ namespace legyenOnIsMilliomos
         {
             var sorkerdesek = new Sorkerdesek();
             var random = new Random();
+            bool helyesSorkerdes = false;
             while (true)
             {
                 var r = random.Next(sorkerdesek.OsszesSorKerdes.Count);
@@ -40,11 +41,12 @@ namespace legyenOnIsMilliomos
 
                 var eltelt = DateTime.Now - start;
 
-                if (valasz == kerdes.HelyesSorrend.ToUpper() && eltelt.TotalSeconds <= 30)
+                if (valasz == kerdes.HelyesSorrend.ToUpper() && eltelt.TotalSeconds <= 300)
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine($"Helyes válasz! ({eltelt.TotalSeconds:0.00} másodperc)");
                     Console.ResetColor();
+                    helyesSorkerdes = true;
                     break;
                 }
                 else
@@ -59,6 +61,114 @@ namespace legyenOnIsMilliomos
                     string ujra = Console.ReadLine().ToLower();
                     if (ujra != "i")
                     {
+                        return;
+                    }
+                }
+            }
+
+            if (helyesSorkerdes)
+            {
+                var kerdesek = new Kerdesek();
+                int[] nyeremenyek = {
+                        10000, 50000, 100000, 250000, 500000,
+                        1000000, 2500000, 5000000, 10000000, 15000000,
+                        20000000, 25000000, 30000000, 40000000, 50000000
+                    };
+                int szint = 1;
+                int nyertOsszeg = 0;
+
+                for (int i = 0; i < 15; i++)
+                {
+                    List<Kerdes> szintKerdesek = new List<Kerdes>();
+                    foreach (Kerdes k in kerdesek.OsszesKerdes)
+                    {
+                        if (k.Szint == szint)
+                        {
+                            szintKerdesek.Add(k);
+                        }
+                    }
+
+                    var kerdes = szintKerdesek[random.Next(szintKerdesek.Count)];
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"\n{szint}. kérdés - {nyeremenyek[i]} Ft");
+                    Console.ResetColor();
+                    Console.Write("Téma: ");
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine(kerdes.Kategoria);
+
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(kerdes.KerdesSzoveg);
+
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    char[] betuk = { 'A', 'B', 'C', 'D' };
+                    for (int j = 0; j < kerdes.Valasz.Count; j++)
+                    {
+                        Console.WriteLine($"{betuk[j]}) {kerdes.Valasz[j]}");
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("Válasz (Pl: A): ");
+                    string valasz = Console.ReadLine()?.Trim().ToUpper() ?? "";
+
+                    if (valasz == kerdes.HelyesValasz.ToUpper())
+                    {
+                        nyertOsszeg = nyeremenyek[i];
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Helyes válasz!");
+                        Console.ResetColor();
+
+                        if (szint == 15)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"Gratulálok! Megnyerted a fődíjat: {nyertOsszeg} Ft!");
+                            Console.ResetColor();
+                            break;
+                        }
+
+                        Console.Write("Szeretnéd folytatni a játékot? (i/n): ");
+                        string folytat = Console.ReadLine().ToLower();
+                        if (folytat != "i")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"Nyert összeged: {nyertOsszeg} Ft");
+                            Console.ResetColor();
+                            break;
+                        }
+                        szint++;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Rossz válasz!");
+                        Console.WriteLine($"A helyes válasz ez lett volna: {kerdes.HelyesValasz}");
+                        Console.ResetColor();
+
+                        if (szint > 10)
+                        {
+                            nyertOsszeg = nyeremenyek[9];
+                        }
+                        else if (szint > 5)
+                        {
+                            nyertOsszeg = nyeremenyek[4];
+                        }
+                        else
+                        {
+                            nyertOsszeg = 0;
+                        }
+
+                        if (nyertOsszeg > 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"Biztos nyereményed: {nyertOsszeg} Ft");
+                            Console.ResetColor();
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Sajnos nem nyertél semmit.");
+                            Console.ResetColor();
+                        }
                         break;
                     }
                 }
